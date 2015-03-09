f384034330b2b02d1da8bfba37adf1719e52c82e
martin butler <martinebutler@gmail.com>
2015-03-09 18:11:29 -0400

    set up table and init pagination

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,15 +1,7 @@
 'use strict';
 
 angular.module('simonsFoundationApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.searchableBy = [{'field': 'chrom', 
-      'selectTitle': 'Chromosome',
-      'description': 'Reference sequence chromosome or scaffold'
-    }, {'field': 'name', 
-      'selectTitle': 'Name',
-      'description': 'Name of gene',
-      'typeAheadData': ''
-    }];
+  .controller('MainCtrl', function ($scope, $http, ngTableParams) {
     $http.get('/api/genedata/distinctChrom/').success(function(chroms) {
       var reA = /[^a-zA-Z]/g;
       var reN = /[^0-9]/g;
@@ -25,10 +17,33 @@ angular.module('simonsFoundationApp')
         }
       })
     });
+
+    $scope.searchableBy = [{'field': 'chrom', 
+      'selectTitle': 'Chromosome',
+      'description': 'Reference sequence chromosome or scaffold'
+    }, {'field': 'name', 
+      'selectTitle': 'Name',
+      'description': 'Name of gene',
+      'typeAheadData': ''
+    }];
     $scope.searchBy = $scope.searchableBy[0];
+    var data = [];
+
     $scope.getrefGeneData = function(field, criteria) {
-      $http.get('/api/genedata/search/' + field + '/' + criteria).success(function(data) {
+      $http.get('/api/genedata/search/' + field + '/' + criteria).success(function(geneData) {
+        data = geneData;
+        $scope.tableParams.reload();
       });
     }
+
+    $scope.tableParams = new ngTableParams({
+        page: 1,            // show first page
+        count: 10           // count per page
+      }, {
+        total: data.length, // length of data
+        getData: function($defer, params) {
+            $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+      }
+    });
 
   });
