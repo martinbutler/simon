c8cabedc78ac5526bbdcb71612d69158ef6238fa
martin butler <martinebutler@gmail.com>
2015-03-10 12:38:34 -0400

    comments. refactor

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,6 +2,7 @@
 
 angular.module('simonsFoundationApp')
   .controller('MainCtrl', function ($scope, $http, ngTableParams, $filter) {
+    // get the distinct values in the chrom field for typeahead
     $http.get('/api/genedata/distinctChrom/').success(function(chroms) {
       var reA = /[^a-zA-Z]/g;
       var reN = /[^0-9]/g;
@@ -18,6 +19,7 @@ angular.module('simonsFoundationApp')
       })
     });
 
+    // selectable fields and attributes to search on
     $scope.searchableBy = [{
       'field': 'chrom', 
       'selectTitle': 'Chromosome',
@@ -28,6 +30,9 @@ angular.module('simonsFoundationApp')
       'description': 'Name of gene',
       'typeAheadData': ''
     }];
+    $scope.searchBy = $scope.searchableBy[0];  // default searchby field
+
+    // user selectable css style options
     $scope.userStyles = [{
       'title': 'Default',
       'css_class': 'searchDefault'
@@ -41,18 +46,17 @@ angular.module('simonsFoundationApp')
       'title': 'Blues',
       'css_class': 'searchBlues'
     }]
-    $scope.searchBy = $scope.searchableBy[0];
-    var data = [];
-    $scope.haveData = false;
-    $scope.userStyle = $scope.userStyles[0];
-
+    $scope.userStyle = $scope.userStyles[0];  // default css style for table
+    
+    var data = [];  // init search results array 
+    // get search results
     $scope.getrefGeneData = function(field, criteria) {
       $http.get('/api/genedata/search/' + field + '/' + criteria).success(function(geneData) {
+        // validate results returns records, if not provide error msg to user
         if(geneData.length > 0) {
           data = geneData;
           $scope.tableParams.reload();
           $scope.tableParams.total(data.length);
-          $scope.haveData = true;
           $scope.searchError = false;
         } else {
           $scope.searchError = "ERROR: No records found for '" + criteria + "'";
@@ -60,6 +64,7 @@ angular.module('simonsFoundationApp')
       });
     }
 
+    // table object, includes sorting and pagination
     $scope.tableParams = new ngTableParams({
       page: 1,            // show first page
       count: 10,          // count per page
@@ -76,8 +81,12 @@ angular.module('simonsFoundationApp')
       }
     });
 
+    // user input validation
     $scope.notValid = true;
     $scope.validateCriteria = function(crit) {
+      // will check if there is a match in typeAheadData for chrom
+      // will ensure that at least one character has been entered for 
+      //    fields with no typeahead data
       if(crit.length === 0 || $scope.searchBy.typeAheadData && $scope.searchBy.typeAheadData.indexOf(crit) < 0) {
         $scope.notValid = true;
       } else {
@@ -85,12 +94,14 @@ angular.module('simonsFoundationApp')
       }
     }
 
+    // check if user pressed enter and run search
     $scope.keyPressed = function(keyEvent) {
       if (keyEvent.which === 13 && !$scope.notValid) {
         $scope.getrefGeneData($scope.searchBy.field, $scope.criteria);
       }
     }
 
+    // CSV export of data
     $scope.csvHeader=['chrom', 'name', 'txEnd', 'txStart'];
     $scope.getData = function() {
       return data;
